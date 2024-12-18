675c8716c8ea901ac681de2711a8e334a373c28a
alimohammad <alimohammad1995@gmail.com>
2024-12-18 19:18:30 +0100

    better

--- oracle/main.go
+++ oracle/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
-	"github.com/blocto/solana-go-sdk/common"
+	"log"
+
+	"gringotts/models"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/viper"
-	"gringotts/models"
-	"log"
 )
 
 func main() {
@@ -22,20 +22,6 @@ func main() {
 	if err := models.LoadAccounts(); err != nil {
 		log.Fatal(err)
 	}
-
-	programID := common.PublicKeyFromString("D4qgtRF5t7MzA5nASfag3ef5Y68xb5hBcp9ac9Wttfkr")
-
-	seed1 := []byte("Peer")
-	bytes := make([]byte, 4)
-	binary.LittleEndian.PutUint32(bytes, 40231)
-
-	// Derive the PDA
-	pda, bump, err := common.FindProgramAddress(
-		[][]byte{seed1, bytes},
-		programID,
-	)
-
-	fmt.Println(pda, bump, err)
 
 	app := fiber.New()
 	registerRoutes(app)
